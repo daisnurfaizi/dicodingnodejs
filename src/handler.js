9baa917768436b0000def2d654f5c02eba056560
daisnurfaizi <dsnurfaizi@gmail.com>
2022-06-24 23:07:27 +0700

    fix bug

--- src/handler.js
+++ src/handler.js
@@ -31,6 +31,87 @@ const addNoteHandler = (request, h)=>{
   response.code(400);
   return response;
 };
+const getAllNotesHandler = ()=>({
+  status: 'success',
+  message: 'Catatan berhasil ditampilkan',
+  data: {
+    notes,
+  },
+});
+
+const getNoteHandler = (request, h)=>{
+  const {id} = request.params;
+  const note = notes.find((note)=>note.id === id);
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      message: 'Catatan berhasil ditampilkan',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditampilkan',
+  });
+  response.code(400);
+  return response;
+};
+
+const editNoteHandler = (request, h)=>{
+  const {id} = request.params;
+  const {title, tags, body} = request.payload;
+  const updatedAt = new Date().toISOString();
+
+  const noteIndex = notes.findIndex((note)=>note.id === id);
+  if (noteIndex !== -1) {
+    notes[noteIndex] = {
+      ...notes[noteIndex],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+    return {
+      status: 'success',
+      message: 'Catatan berhasil diedit',
+      data: {
+        noteId: id,
+      },
+    };
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal diedit',
+  });
+  response.code(400);
+  return response;
+};
+
+const deleteNoteHandler = (request, h)=>{
+  const {id} = request.params;
+  const noteIndex = notes.findIndex((note)=>note.id === id);
+  if (noteIndex !== -1) {
+    notes.splice(noteIndex, 1);
+    return {
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+      data: {
+        noteId: id,
+      },
+    };
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus',
+  });
+  response.code(400);
+  return response;
+};
 
 
-module.exports = {addNoteHandler};
+// eslint-disable-next-line max-len
+module.exports = {addNoteHandler, getAllNotesHandler, getNoteHandler, editNoteHandler, deleteNoteHandler};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,30 @@
-const {addNoteHandler} = require('./handler');
+const {addNoteHandler, getAllNotesHandler, getNoteHandler, editNoteHandler, deleteNoteHandler} = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteHandler,
   },
 ];
 
